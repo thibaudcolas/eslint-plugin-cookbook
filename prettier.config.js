3a3c89f71dd1718e357bdb5725703caf5defe9b1
Thibaud Colas <thibaudcolas@gmail.com>
2017-08-28 23:54:48 +0300

    chore(qa): move single prettier.config.js declaration at the root

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,18 @@
-module.exports = require('./lib/prettier.config');
+// https://github.com/prettier/prettier#api
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    semicolons: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
