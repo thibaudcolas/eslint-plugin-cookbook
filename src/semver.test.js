ceebbe5ba26eda6b628a299023ab242184f9e116
Thibaud Colas <thibaudcolas@gmail.com>
2018-09-20 01:40:00 +0300

    test(config): add snapshot tests of whole config

--- src/semver.test.js
+++ src/semver.test.js
@@ -1,12 +1,23 @@
 const pkg = require("../package.json");
+const config = require("./recommended.json");
 
 describe("semver - should those tests break, consider releasing a new major version of the package", () => {
   it("dependencies", () => {
-    expect(pkg.dependencies).toMatchSnapshot();
+    expect(pkg.dependencies).toMatchInlineSnapshot(`
+Object {
+  "eslint-plugin-import": "^2.14.0",
+  "eslint-plugin-jsx-a11y": "^6.1.1",
+  "eslint-plugin-react": "^7.11.1",
+}
+`);
   });
 
   it("peerDependencies", () => {
-    expect(pkg.peerDependencies).toMatchSnapshot();
+    expect(pkg.peerDependencies).toMatchInlineSnapshot(`
+Object {
+  "eslint": "^5.6.0",
+}
+`);
   });
 
   it("devDependencies", () => {
@@ -16,6 +27,16 @@ describe("semver - should those tests break, consider releasing a new major vers
         deps[key] = pkg.devDependencies[key];
         return deps;
       }, {});
-    expect(devDependencies).toMatchSnapshot();
+    expect(devDependencies).toMatchInlineSnapshot(`
+Object {
+  "eslint": "^5.6.0",
+  "eslint-config-airbnb": "^17.1.0",
+  "eslint-config-prettier": "^3.0.1",
+}
+`);
+  });
+
+  it("config", () => {
+    expect(config).toMatchSnapshot();
   });
 });
