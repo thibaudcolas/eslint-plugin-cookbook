b046648bd32e7428a4bb1763f3ccc9198868e5af
Thibaud Colas <thibaudcolas@gmail.com>
2018-09-20 02:03:26 +0300

    feat(config): cleanup config rules
    BREAKING CHANGE: Updates how rules are configred in the config, generally to be stricter. Here are the detailed changes:
    
    ```diff
    "import/no-extraneous-dependencies": Array [
      2,
      Object {
    -     "devDependencies": true,
    +     "devDependencies": Array [
    +       "test/**",
    +       "tests/**",
    +       "spec/**",
    +       "**/__tests__/**",
    +       "**/__mocks__/**",
    +       "test.{js,jsx}",
    +       "test-*.{js,jsx}",
    +       "**/*{.,_}{test,spec}.{js,jsx}",
    +       "**/jest.config.js",
    +       "**/vue.config.js",
    +       "**/webpack.config.js",
    +       "**/webpack.config.*.js",
    +       "**/rollup.config.js",
    +       "**/rollup.config.*.js",
    +       "**/gulpfile.js",
    +       "**/gulpfile.*.js",
    +       "**/Gruntfile{,.js}",
    +       "**/protractor.conf.js",
    +       "**/protractor.conf.*.js",
    +       "**/dangerfile.js",
    +    ],
      },
    ],
    
    - "import/prefer-default-export": "off",
    + "import/prefer-default-export": "error",
    
    - "jsx-a11y/label-has-associated-control": "error",
    + "jsx-a11y/label-has-associated-control": "off",
    
    - "react/jsx-boolean-value": "off",
    + "react/jsx-boolean-value": "error",
    
    - "react/jsx-indent": "error",
    + "react/jsx-indent": "off",
    
    - "react/jsx-indent-props": "error",
    + "react/jsx-indent-props": "off",
    
    - "react/sort-comp": "off",
    + "react/sort-comp": "error",
    
    - "arrow-body-style": "off",
    + "arrow-body-style": "error",
    
    - "eol-last": "warn",
    + "eol-last": "off",
    
    - "id-length": "off",
    + "id-length": "off",
    
    - "indent": "error",
    + "indent": "off",
    
    - "jsx-quotes": "warn"
    + "jsx-quotes": "off"
    
    - "max-len": "warn"
    + "max-len": "off"
    
    "no-console": Array [
        "warn",
    -   Object {
    -     "allow": Array [
    -       "warn",
    -       "error",
    -       "time",
    -       "timeEnd",
    -     ],
    -   },
    ],
    
    - "no-new": "off"
    + "no-new": "error",
    
    - "no-plusplus": "off"
    + "no-plusplus": "error",
    
    "object-shorthand": Array [
        "error",
    -   "methods",
    +   "always",
    +   Object {
    +     "avoidQuotes": true,
    +     "ignoreConstructors": false,
    +   },
    ],
    ```

--- src/base.js
+++ src/base.js
@@ -1,52 +1,64 @@
+const BASE = {
+  // Temporary warning comments are ok.
+  "no-warning-comments": [
+    1,
+    { terms: ["todo", "fixme", "xxx"], location: "start" },
+  ],
+};
+
+const IMPORTS = {
+  "import/no-extraneous-dependencies": [
+    2,
+    {
+      devDependencies: [
+        "test/**",
+        "tests/**",
+        "spec/**",
+        "**/__tests__/**",
+        "**/__mocks__/**",
+        "test.{js,jsx}",
+        "test-*.{js,jsx}",
+        "**/*{.,_}{test,spec}.{js,jsx}",
+        "**/jest.config.js",
+        "**/vue.config.js",
+        "**/webpack.config.js",
+        "**/webpack.config.*.js",
+        "**/rollup.config.js",
+        "**/rollup.config.*.js",
+        "**/gulpfile.js",
+        "**/gulpfile.*.js",
+        "**/Gruntfile{,.js}",
+        "**/protractor.conf.js",
+        "**/protractor.conf.*.js",
+        "**/dangerfile.js",
+      ],
+      optionalDependencies: false,
+    },
+  ],
+};
+
+const REACT = {
+  // We do not want files whose content might change to have to be renamed.
+  "react/jsx-filename-extension": [2, { extensions: [".js"] }],
+};
+
+const LEGACY = {
+  // Does not work at the moment for nested fields.
+  // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
+  "jsx-a11y/label-has-for": [0],
+  "jsx-a11y/label-has-associated-control": [0],
+  // Do not enforce reassignment to properties of object parameters.
+  "no-param-reassign": [2, { props: false }],
+  // Only forbid "any" proptype, no other.
+  "react/forbid-prop-types": [2, { forbid: ["any"] }],
+  // Disable requirement to not use bind.
+  "react/jsx-no-bind": [0],
+};
+
 module.exports = {
   extends: ["airbnb", "prettier", "prettier/flowtype", "prettier/react"],
 
-  rules: {
-    indent: [2, 4],
-    "react/jsx-indent": [2, 4],
-    "react/jsx-indent-props": [2, 4],
-    "max-len": [1, 120, 4, { ignoreUrls: true }],
-    "id-length": [0],
-    // Only enforce object shorthand for methods.
-    "object-shorthand": [2, "methods"],
-    // Do not enforce arrow body style.
-    "arrow-body-style": [0],
-    // Do not enforce reassignment to properties of object parameters.
-    "no-param-reassign": [2, { props: false }],
-    "no-new": [1],
-    "eol-last": [1],
-    "no-multi-spaces": [0],
-    "no-warning-comments": [
-      1,
-      { terms: ["todo", "fixme", "xxx"], location: "start" },
-    ],
-    // It's ok to use i++.
-    "no-plusplus": [0],
-    "no-console": [1, { allow: ["warn", "error", "time", "timeEnd"] }],
-    "jsx-quotes": [1, "prefer-double"],
-    // Disable requirement to sort React component methods.
-    "react/sort-comp": [0],
-    // Only forbid "any" proptype, no other.
-    "react/forbid-prop-types": [2, { forbid: ["any"] }],
-    // Disable requirement to drop value from boolean props.
-    "react/jsx-boolean-value": [0],
-    // Disable requirement to not use bind.
-    "react/jsx-no-bind": [0],
-    // Only use .js file extension.
-    "react/jsx-filename-extension": [2, { extensions: [".js"] }],
-    "import/no-extraneous-dependencies": [
-      2,
-      {
-        devDependencies: true,
-        optionalDependencies: false,
-        peerDependencies: false,
-      },
-    ],
-    "import/prefer-default-export": [0],
-    // Does not work at the moment for nested fields.
-    // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-for.md
-    "jsx-a11y/label-has-for": [0],
-  },
+  rules: Object.assign({}, BASE, IMPORTS, REACT, LEGACY),
 
   // http://eslint.org/docs/user-guide/configuring#specifying-environments
   env: {
