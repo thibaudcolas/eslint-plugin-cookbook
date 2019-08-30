51178e609a781606f17b13954bf2fbf6cd77a042
Thibaud Colas <thibaudcolas@gmail.com>
2019-08-30 23:41:52 +0100

    chore(qa): update all pre-commit scripts

--- .githooks/pre-commit.7.lint.sh
+++ .githooks/pre-commit.7.lint.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -n "$JS_STAGED" ];
-then
-    npx prettier --check $JS_STAGED
-fi
-
-if [ -n "$SCSS_STAGED" ];
-then
-    npx prettier --check $SCSS_STAGED
-fi
-
-if [ -n "$MD_STAGED" ];
-then
-    npx prettier --check $MD_STAGED
-fi
-
-if [ -n "$JSON_STAGED" ];
-then
-    npx prettier --check $JSON_STAGED
-fi
-
-if [ -n "$YAML_STAGED" ];
-then
-    npx prettier --check $YAML_STAGED
-fi
-
 TRAVIS_STAGED=$(grep -e '.travis.yml$' -e '.travis.yaml$' <<< "$STAGED" || true)
 
 if [ -n "$TRAVIS_STAGED" ];
 then
   travis lint --exit-code $TRAVIS_STAGED
+fi
+
+if [ -n "$JS_STAGED" ];
+then
+  npx eslint --report-unused-disable-directives $JS_STAGED
 fi
--- .githooks/pre-commit.8.test.sh
+++ .githooks/pre-commit.8.test.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 
-set -e
-
 if [ -n "$JS_STAGED" ];
 then
   npm run build
   npm run test
-fi
-
-DANGER_STAGED=$(grep -e 'dangerfile.js$' <<< "$STAGED" || true)
-
-if [ -n "$DANGER_STAGED" ];
-then
-  npx danger local
 fi
