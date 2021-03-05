6f2ab5aa408f956616ed7f5e977108fcbaa6fe42
Thibaud Colas <thibaudcolas@gmail.com>
2021-03-05 00:08:36 +0000

    chore(ci): migrate from Travis to GitHub Actions

--- .githooks/pre-commit.7.lint.sh
+++ .githooks/pre-commit.7.lint.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-TRAVIS_STAGED=$(grep -e '.travis.yml$' -e '.travis.yaml$' <<< "$STAGED" || true)
-
-if [ -n "$TRAVIS_STAGED" ];
-then
-  travis lint --exit-code $TRAVIS_STAGED
-fi
-
 if [ -n "$JS_STAGED" ];
 then
   npx eslint --report-unused-disable-directives $JS_STAGED
